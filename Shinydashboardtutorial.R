92837663c6998e1bd54f19cd24ee9faa96b746af
Santiago Garcia Herrera <sngh966.sg@gmail.com>
2020-04-09 21:22:32 -0500

    Se empezo a usar git en el codigo de shiny

---
+++ Shinydashboardtutorial.R
@@ -0,0 +1,69 @@
+install.packages("shiny")
+install.packages("shinydashboard")
+library(shiny)
+library(shinydashboard)
+header <- dashboardHeader(
+  dropdownMenu(
+    type = "message",
+    messageItem(
+      from = "Santiago",
+      message = "Hola como estas",
+      href = "https://spotthestation.nasa.gov/sightings/"
+    ),
+    messageItem(
+      from = "Lau",
+      message = "Hola que tal la fiesta",
+      href = "https://spotthestation.nasa.gov/sightings/"
+    )
+  ),
+  dropdownMenu(
+        type = "tasks",
+         taskItem(
+           text="Mission Learn Shiny Dashboard",
+           value = 10
+         )
+      ),
+  dropdownMenu(
+    type = "notifications",
+    notificationItem(
+      text = "Debo acostarme mas temprano"
+    )
+  )
+  
+)
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Datos",
+             tabName = "datos"
+      
+    ),
+    menuItem( "Dashboard",
+              tabName = "dashboard")
+  )
+)
+body <- dashboardBody(
+  tabItem( tabName = "Datos")
+)
+
+
+ui <- dashboardPage(header,sidebar,body)
+
+
+
+server <- function(input, output, session) {
+  
+}
+
+shinyApp(ui, server)
+
+
+
+# header <- dashboardHeader(
+#   # Create a tasks drop down menu
+#   dropdownMenu(
+#     type = "tasks",
+#     taskItem(
+#       text="Mission Learn Shiny Dashboard",
+#       value = 10
+#     )
+#   )
